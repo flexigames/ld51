83745f216051d810321d3aa1032c5fcb8439f740
Finn Pauls <mbp@pauls.dev>
2022-10-01 22:55:22 +0200

    Build house with ProBuilder

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -5,8 +5,33 @@ using TMPro;
 
 public class Door : MonoBehaviour, Interactable
 {
+    public TextMeshPro displayMesh;
+    private float counter = 10 * 1000;
 
     public void Interact() {
-        Debug.Log("Door Opened");
+        counter = 0;
+        UpdateText();
+    }
+
+    public void Update() {
+        counter -= Time.deltaTime * 1000.0f;
+        if (counter < 0) {
+            counter = 0;
+        }
+
+        if (counter == 0) {
+            if (Random.value < 0.0008) {
+                counter = 10 * 1000;
+            }
+        }
+        UpdateText();
+    }
+
+    private void UpdateText() {
+        if (counter > 0) {
+            displayMesh.text = string.Format("{0:0.00}", counter / 1000f);
+        } else {
+            displayMesh.text = "";
+        }
     }
 }
