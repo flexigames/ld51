829f0412f7145e56c3fb0a3a2b981b79cb8cb18d
Finn Pauls <mbp@pauls.dev>
2022-10-01 21:26:40 +0200

    Button Counter

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Button : MonoBehaviour, Interactable
 {
+    public TextMeshPro displayMesh;
+    private float counter = 10 * 1000;
+
     public void Interact() {
-        Debug.Log("Button Pressed");
+        counter = 10 * 1000;
+        UpdateText();
+    }
+
+    public void Update() {
+        counter -= Time.deltaTime * 1000.0f;
+        UpdateText();
+    }
+
+    private void UpdateText() {
+        displayMesh.text = string.Format("{0:0.00}", counter / 1000f);
     }
 }
--- Assets/Door.cs
+++ Assets/Door.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Door : MonoBehaviour, Interactable
 {
+
     public void Interact() {
         Debug.Log("Door Opened");
     }
