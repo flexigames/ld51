38ef3afe7e9761b2cfc2c4d36576af6d70ae8b50
Finn Pauls <mbp@pauls.dev>
2022-10-03 14:25:41 +0200

    Show the tasks as a HUD

--- Assets/TaskManager.cs
+++ Assets/TaskManager.cs
@@ -1,30 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TaskManager : MonoBehaviour
 {
-    public List<GameTask> tasks;
+    public TextMeshProUGUI taskText;
 
-    private Dictionary<string, int> completedSteps = new Dictionary<string, int>();
-
+    private List<CompletableTask> tasks = new List<CompletableTask>() {
+        new CompletableTask("books", "Put all books in the shelf", 2),
+        new CompletableTask("kitchen", "Do the dishes", 3),
+        new CompletableTask("laundry", "Pick up laundry from the floor", 3)
+    };
 
     void Start() {
-        foreach (GameTask task in tasks) {
-            completedSteps.Add(task.id, 0);
-        }
+        PrintTasks();
     }
 
     public void IncreaseSteps(string taskId)
     {
-        completedSteps[taskId]++;
+        CompletableTask task = tasks.Find(t => t.id == taskId);
+        task.completedSteps++;
+
         PrintTasks();
     }
 
-    public void PrintTasks() {
-        foreach (GameTask task in tasks) {
-            Debug.Log(task.name + ": " + completedSteps[task.id] + "/" + task.numberOfSteps);
+    public void PrintTasks()
+    {
+        string text = "";
+        foreach (CompletableTask task in tasks) {
+            text += task.name + " " + task.completedSteps + "/" + task.totalSteps + "\n";
         }
+        taskText.text = text;
     }
+}
 
+public class CompletableTask
+{
+    public string id;
+    public string name;
+    public int totalSteps;
+    public int completedSteps = 0;
+
+    public CompletableTask(string id, string name, int totalSteps)
+    {
+        this.id = id;
+        this.name = name;
+        this.totalSteps = totalSteps;
+    }
 }
